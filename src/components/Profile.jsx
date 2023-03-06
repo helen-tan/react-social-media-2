ae5858d3d2f084d54603c7cf90c2f92f0a8f9b4b
helen-tan <helentanmz0909@gmail.com>
2023-03-06 17:27:55 +0800

    create state to store profile data + dynamically display some data

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import { useParams } from "react-router-dom" 
 import Axios from 'axios'
 import StateContext from '../StateContext'
@@ -9,12 +9,21 @@ const Profile = () => {
 
     const globalState = useContext(StateContext)
 
+    const [profileData, setProfileData] = useState({
+        profileUsername: "...",
+        profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
+        isFollowing: false,
+        counts: { postCounts: "", followerCount: "", followingCount: "" }
+    })
+
     useEffect(() => {
         // Fetch data related to the profile (specified by username)
         async function fetchData() {
             try {   
                 const response = await Axios.post(`/profile/${username}`, { token: globalState.user.token })
-                console.log(response.data)
+                // console.log(response.data)
+
+                setProfileData(response.data)
             } catch (err) {
                 console.log("There was a problem.")
             }
@@ -26,19 +35,19 @@ const Profile = () => {
     return (
         <Page title="Profile Screen">
             <h2>
-                <img className="avatar-small" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> brad
+                <img className="avatar-small" src={profileData.profileAvatar} /> {profileData.profileUsername}
                 <button className="btn btn-primary btn-sm ml-2">Follow <i className="fas fa-user-plus"></i></button>
             </h2>
 
             <div className="profile-nav nav nav-tabs pt-2 mb-4">
                 <a href="#" className="active nav-item nav-link">
-                    Posts: 3
+                    Posts: {profileData.counts.postCounts}
                 </a>
                 <a href="#" className="nav-item nav-link">
-                    Followers: 101
+                    Followers: {profileData.counts.followerCount}
                 </a>
                 <a href="#" className="nav-item nav-link">
-                    Following: 40
+                    Following: {profileData.counts.followingCount}
                 </a>
             </div>
 
