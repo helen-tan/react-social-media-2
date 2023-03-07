0c521aef831d3c3bb1c83f5c03027988e1be5f91
helen-tan <helentanmz0909@gmail.com>
2023-03-07 11:30:07 +0800

    Make axios request to get a user's posts

--- src/components/ProfilePosts.jsx
+++ src/components/ProfilePosts.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import Axios from 'axios'
 
 const ProfilePosts = () => {
+    const [loading, setLoading] = useState(true)
+    const [posts, setPosts] = useState([])
+
+    const { username } = useParams() 
+
+    useEffect(() => {
+        async function fetchPosts() {
+            try {
+                const response = await Axios.get(`/profile/${username}/posts`)
+                // console.log(response.data)
+                setPosts(response.data)
+                setLoading(false)
+            } catch (err) {
+                console.log("There was a problem.")
+            }
+        }
+
+        fetchPosts()
+    }, [])
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
     return (
         <div className="list-group">
             <a href="#" className="list-group-item list-group-item-action">
