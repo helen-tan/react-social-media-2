5b56707ca5da37d1e5dc6e2c266c5243951d8682
helen-tan <helentanmz0909@gmail.com>
2023-03-07 11:22:41 +0800

    Created ProfilePosts component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useContext, useState } from 'react'
-import { useParams } from "react-router-dom" 
+import { useParams } from "react-router-dom"
 import Axios from 'axios'
 import StateContext from '../StateContext'
 import Page from './Page'
+import ProfilePosts from './ProfilePosts'
 
 const Profile = () => {
     const { username } = useParams() // Get the username segment of the url
@@ -19,7 +20,7 @@ const Profile = () => {
     useEffect(() => {
         // Fetch data related to the profile (specified by username)
         async function fetchData() {
-            try {   
+            try {
                 const response = await Axios.post(`/profile/${username}`, { token: globalState.user.token })
                 // console.log(response.data)
 
@@ -51,20 +52,7 @@ const Profile = () => {
                 </a>
             </div>
 
-            <div className="list-group">
-                <a href="#" className="list-group-item list-group-item-action">
-                    <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #1</strong>
-                    <span className="text-muted small">on 2/10/2020 </span>
-                </a>
-                <a href="#" className="list-group-item list-group-item-action">
-                    <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #2</strong>
-                    <span className="text-muted small">on 2/10/2020 </span>
-                </a>
-                <a href="#" className="list-group-item list-group-item-action">
-                    <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #3</strong>
-                    <span className="text-muted small">on 2/10/2020 </span>
-                </a>
-            </div>
+            <ProfilePosts />
 
         </Page>
     )
