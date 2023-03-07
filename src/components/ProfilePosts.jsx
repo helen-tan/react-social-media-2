3553abcb945bb228dbdff0d1e8e16e5649a96447
helen-tan <helentanmz0909@gmail.com>
2023-03-07 11:39:51 +0800

    Added Link to each post to navigate to a single post

--- src/components/ProfilePosts.jsx
+++ src/components/ProfilePosts.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import Axios from 'axios'
 
 const ProfilePosts = () => {
@@ -33,10 +33,10 @@ const ProfilePosts = () => {
                 const date = new Date(post.createdDate)
                 const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
                 return (
-                    <a key={post._id} href="#" className="list-group-item list-group-item-action">
+                    <Link key={post._id} to={`/post/${post._id}`} className="list-group-item list-group-item-action">
                         <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong> {" "}
                         <span className="text-muted small">{dateFormatted} </span>
-                    </a>
+                    </Link>
                 )
             })}
         </div>
