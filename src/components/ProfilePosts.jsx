c4ea77d4fa76a90e5db5a2a1c14173281e820e8d
helen-tan <helentanmz0909@gmail.com>
2023-03-07 11:37:12 +0800

    Display dynamic posts data

--- src/components/ProfilePosts.jsx
+++ src/components/ProfilePosts.jsx
@@ -6,7 +6,7 @@ const ProfilePosts = () => {
     const [loading, setLoading] = useState(true)
     const [posts, setPosts] = useState([])
 
-    const { username } = useParams() 
+    const { username } = useParams()
 
     useEffect(() => {
         async function fetchPosts() {
@@ -29,18 +29,16 @@ const ProfilePosts = () => {
 
     return (
         <div className="list-group">
-            <a href="#" className="list-group-item list-group-item-action">
-                <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #1</strong>
-                <span className="text-muted small">on 2/10/2020 </span>
-            </a>
-            <a href="#" className="list-group-item list-group-item-action">
-                <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #2</strong>
-                <span className="text-muted small">on 2/10/2020 </span>
-            </a>
-            <a href="#" className="list-group-item list-group-item-action">
-                <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #3</strong>
-                <span className="text-muted small">on 2/10/2020 </span>
-            </a>
+            {posts.map(post => {
+                const date = new Date(post.createdDate)
+                const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+                return (
+                    <a key={post._id} href="#" className="list-group-item list-group-item-action">
+                        <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong> {" "}
+                        <span className="text-muted small">{dateFormatted} </span>
+                    </a>
+                )
+            })}
         </div>
     )
 }
