3595318be6ff1df129656610c5897092eaff5e92
helen-tan <helentanmz0909@gmail.com>
2023-03-08 11:02:32 +0800

    Used a Reducer setup for EditPost

--- src/components/EditPost.jsx
+++ src/components/EditPost.jsx
@@ -1,26 +1,59 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import Axios from 'axios'
 import Page from './Page'
 import LoadingDotsIcon from './LoadingDotsIcon'
-import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
-import { Tooltip } from 'react-tooltip'
-import 'react-tooltip/dist/react-tooltip.css';
 
 const EditPost = () => {
-    const [loading, setLoading] = useState(true)
-    const [post, setPost] = useState()
-    const { id } = useParams()
+    const originalState = {
+        title: {
+            value: "",
+            hasErrors: false,
+            message: ""
+        },
+        body: {
+            value: "",
+            hasErrors: false,
+            message: ""
+        },
+        isFetching: true,
+        isSaving: false,
+        id: useParams().id,
+        sendCount: 0
+    }
+
+    function ourReducer(state, action) {
+        switch (action.type) {
+            case "fetchComplete":
+                return {
+                    title: {
+                        value: action.value.title,
+                        hasErrors: false,
+                        message: ""
+                    },
+                    body: {
+                        value: action.value.body,
+                        hasErrors: false,
+                        message: ""
+                    },
+                    isFetching: false,
+                    isSaving: false,
+                    id: state.id,
+                    sendCount: 0
+                }
+        }
+    }
+
+    const [state, dispatch] = useReducer(ourReducer, originalState)
 
     useEffect(() => {
         const ourRequest = Axios.CancelToken.source() // A way of identifying an Axios request
 
         async function fetchPost() {
             try {
-                const response = await Axios.get(`/post/${id}`, { cancelToken: ourRequest.token })
+                const response = await Axios.get(`/post/${state.id}`, { cancelToken: ourRequest.token })
                 // console.log(response.data)
-                setPost(response.data)
-                setLoading(false)
+                dispatch({ type: "fetchComplete", value: response.data })
             } catch (err) {
                 console.log("There was a problem, or the request was cancelled.")
             }
@@ -35,16 +68,13 @@ const EditPost = () => {
         }
     }, [])
 
-    if (loading) {
+    if (state.isFetching) {
         return (
             <Page title="...">
                 <LoadingDotsIcon />
             </Page>
         )
     }
-
-    const date = new Date(post.createdDate)
-    const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
 
     return (
         <Page title="Edit Post">
@@ -53,14 +83,14 @@ const EditPost = () => {
                     <label htmlFor="post-title" className="text-muted mb-1">
                         <small>Title</small>
                     </label>
-                    <input autoFocus value={post.title} name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+                    <input autoFocus value={state.title.value} name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="post-body" className="text-muted mb-1 d-block">
                         <small>Body Content</small>
                     </label>
-                    <textarea name="body" id="post-body" value={post.body} className="body-content tall-textarea form-control" type="text" />
+                    <textarea name="body" id="post-body" value={state.body.value} className="body-content tall-textarea form-control" type="text" />
                 </div>
 
                 <button className="btn btn-primary">Confirm Changes</button>
