6a4aa34166e55129926d9e875db9219a15409146
helen-tan <helentanmz0909@gmail.com>
2023-03-09 09:58:31 +0800

    Added a catch all route for non-existent urls

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import ViewSinglePost from "./components/ViewSinglePost";
 import FlashMessages from "./components/FlashMessages";
 import Profile from "./components/Profile";
 import EditPost from "./components/EditPost";
+import NotFound from "./components/NotFound";
 
 // Context
 import StateContext from "./StateContext";
@@ -94,6 +95,9 @@ function App() {
             <Route path="/post/:id" element={<ViewSinglePost />} />
             <Route path="/post/:id/edit" element={<EditPost/>} />
             <Route path="/profile/:username/*" element={<Profile />} />
+
+            {/* Catch all */}
+            <Route path="*" element={<NotFound />}/>
           </Routes>
 
           <Footer />
