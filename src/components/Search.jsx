196fadd0086b36cb3f52771c78139f58fe310f8b
helen-tan <helentanmz0909@gmail.com>
2023-03-10 10:11:57 +0800

    Close the Search overlay after clicking into a post

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -118,12 +118,11 @@ const Search = () => {
                                 const date = new Date(post.createdDate)
                                 const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
                                 return (
-                                    <Link key={post._id} to={`/post/${post._id}`} className="list-group-item list-group-item-action">
+                                    <Link onClick={() => globalDispatch({ type: "closeSearch" })} key={post._id} to={`/post/${post._id}`} className="list-group-item list-group-item-action">
                                         <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong> {" "}
                                         <span className="text-muted small">by {post.author.username} {dateFormatted} </span>
                                     </Link>
                                 )
-
                             })}
                         </div>
                     </div>
