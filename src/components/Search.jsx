290320ce307b9ec0f98447b95c4ecba599eef068
helen-tan <helentanmz0909@gmail.com>
2023-03-10 10:02:22 +0800

    Search: Show loading spinner when typing + show fetched results after 3000ms

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react'
-import Axios  from 'axios'
+import Axios from 'axios'
 import DispatchContext from '../DispatchContext'
 import { useImmer } from 'use-immer'
 
@@ -13,7 +13,7 @@ const Search = () => {
         requestCount: 0
     })
 
-    useEffect(() =>{
+    useEffect(() => {
         // Add keyboard event listener to browser (fn runs when ANY key is pressed)
         document.addEventListener("keyup", searchKeyPressHandler)
 
@@ -26,19 +26,29 @@ const Search = () => {
     // Send a req to the server only after some time after typing (not on every keystroke to avoid flooding the server)
     // Watch state.searchTerm for changes
     useEffect(() => {
-        const delay = setTimeout(() => {
-            //console.log(state.searchTerm)
-            setState(draft => { draft.requestCount++ })
-        }, 3000)
+        if (state.searchTerm.trim()) {
+            setState(draft => {
+                draft.show = "loading"
+            })// Show loading icon
 
-        // Clean up fn not only runs when the component unmounts,
-        // but also the next time when the useEffect runs
-        return () => clearTimeout(delay)
-        // cancels the setTimeout fn above
+            const delay = setTimeout(() => {
+                //console.log(state.searchTerm)
+                setState(draft => { draft.requestCount++ })
+            }, 3000)
 
-        // type 1 char > state.earchTerm changes > useEffect runs > setTimeout runs > quickly type another char > state.searchTerm changes > before useEffect runs again, the 1st instance will run its cleanup return fn 
-        // When a person is typing at a reasonable pace, each keystroke will keep clearing the timeout
-        // and setTimeout fn will only run after they stop typing for the ms specified
+            // Clean up fn not only runs when the component unmounts,
+            // but also the next time when the useEffect runs
+            return () => clearTimeout(delay)
+            // cancels the setTimeout fn above
+
+            // type 1 char > state.earchTerm changes > useEffect runs > setTimeout runs > quickly type another char > state.searchTerm changes > before useEffect runs again, the 1st instance will run its cleanup return fn 
+            // When a person is typing at a reasonable pace, each keystroke will keep clearing the timeout
+            // and setTimeout fn will only run after they stop typing for the ms specified
+        } else {
+            setState(draft => {
+                draft.show = 'neither'
+            })
+        }
     }, [state.searchTerm])
 
     // Watch state.requestCount for changes
@@ -46,13 +56,14 @@ const Search = () => {
         // so that this won't run when component first renders
         if (state.requestCount) {
             const ourRequest = Axios.CancelToken.source() // create cancel token to cancel req if component unmounts in the middle of the req
-            
+
             async function fetchResults() {
                 try {
                     const response = await Axios.post('/search', { searchTerm: state.searchTerm }, { cancelToken: ourRequest.token })
                     // console.log(response.data)
                     setState(draft => {
                         draft.results = response.data
+                        draft.show = "results" // makes results visible
                     })
                 } catch (err) {
                     console.log("There was a problem or the request was cancelled.")
@@ -94,7 +105,9 @@ const Search = () => {
 
             <div className="search-overlay-bottom">
                 <div className="container container--narrow py-3">
-                    <div className="live-search-results live-search-results--visible">
+                    <div className={"circle-loader " + (state.show === "loading" ? "circle-loader--visible" : "")}></div>
+
+                    <div className={"live-search-results " + (state.show == "results" ? "live-search-results--visible" : "")}>
                         <div className="list-group shadow-sm">
                             <div className="list-group-item active"><strong>Search Results</strong> (3 items found)</div>
                             <a href="#" className="list-group-item list-group-item-action">
