3c5f47e0077b12c306e05e50dd2f40e6417ac739
helen-tan <helentanmz0909@gmail.com>
2023-03-10 10:08:58 +0800

    Populate search results with actual fetched results

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import Axios from 'axios'
 import DispatchContext from '../DispatchContext'
 import { useImmer } from 'use-immer'
@@ -105,23 +106,25 @@ const Search = () => {
 
             <div className="search-overlay-bottom">
                 <div className="container container--narrow py-3">
+                    {/* Loading Spinner */}
                     <div className={"circle-loader " + (state.show === "loading" ? "circle-loader--visible" : "")}></div>
-
+                    {/* Search Results */}
                     <div className={"live-search-results " + (state.show == "results" ? "live-search-results--visible" : "")}>
                         <div className="list-group shadow-sm">
-                            <div className="list-group-item active"><strong>Search Results</strong> (3 items found)</div>
-                            <a href="#" className="list-group-item list-group-item-action">
-                                <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #1</strong>
-                                <span className="text-muted small">by brad on 2/10/2020 </span>
-                            </a>
-                            <a href="#" className="list-group-item list-group-item-action">
-                                <img className="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128" /> <strong>Example Post #2</strong>
-                                <span className="text-muted small">by barksalot on 2/10/2020 </span>
-                            </a>
-                            <a href="#" className="list-group-item list-group-item-action">
-                                <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #3</strong>
-                                <span className="text-muted small">by brad on 2/10/2020 </span>
-                            </a>
+                            <div className="list-group-item active">
+                                <strong>Search Results</strong> ({state.results.length} {state.results.length > 1 ? "items" : "item"} found)
+                            </div>
+                            {state.results.map(post => {
+                                const date = new Date(post.createdDate)
+                                const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+                                return (
+                                    <Link key={post._id} to={`/post/${post._id}`} className="list-group-item list-group-item-action">
+                                        <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong> {" "}
+                                        <span className="text-muted small">by {post.author.username} {dateFormatted} </span>
+                                    </Link>
+                                )
+
+                            })}
                         </div>
                     </div>
                 </div>
